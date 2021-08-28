01dbf2a8ab2423dfe0cf8065a6538eca12191940
Lygyane Lima <Lygyane Lima@LYGYANELIMA-PC>
2021-08-27 22:03:34 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosLP_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExerciciosLP_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            try
+            {
+                Console.Write("Digite um número: ");
+                numero = int.Parse(Console.ReadLine());
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine("Não foi possivel converter o dado informado.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Final da execução do programa!");
+            }
+        }
+    }
+}
