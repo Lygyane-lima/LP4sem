e28410b22ce8d9b311e3ee6df18258be15d5ff16
Lygyane-lima <lygy_2712@hotmail.com>
2021-08-27 22:46:14 -0300

    update

---
+++ Exercicio_Variaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio_Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.141459;
+            double raio, area = 0.0;
+
+            Console.Write("Digite o raio do circulo: ");
+            raio = double.Parse(Console.ReadLine());
+
+            area = PI * (raio * raio);
+
+            Console.Write("Área do Círculo : " + area);
+        }
+    }
+}
